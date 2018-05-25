44d1ad03d576ebf0df8f56091ea9802f79029cbf
Anthony Lam <al3101@nyu.edu>
2018-05-25 16:58:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,11 +76,12 @@ class Student
     sql = <<-SQL
     SELECT * FROM students 
     WHERE grade = 10 
+    LIMIT 1 
     SQL
     
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row) 
-    end.first 
+    end[0]
   end 
   
   def self.all_students_in_grade_X(x) 
