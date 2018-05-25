348d248922fb80402b681eb826cc3ddf14e57c92
Anthony Lam <al3101@nyu.edu>
2018-05-25 16:44:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
   end 
   
   def self.students_below_12th_grade 
-    sql <<-SQL
+    sql = <<-SQL
     SELECT * FROM students 
     WHERE grade < 12 
     SQL
@@ -61,7 +61,7 @@ class Student
   end 
   
   def self.first_X_students_in_grade_10(x)
-    sql <<-SQL
+    sql = <<-SQL
     SELECT * FROM students 
     WHERE grade = 10
     LIMIT ? 
